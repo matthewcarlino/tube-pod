4fb5e78e7dbac73eb48636005fff646798076ad9
Matthew Carlino <matthew@blu.shortofcode.net>
2020-10-02 15:46:50 -0400

    download an audio file

--- tube-pod.js
+++ tube-pod.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
-import ytdl from 'ytdl-core'
+import ytdl from 'ytdl-core';
+import fs from 'fs';
 
-const url = '' //put a url in here.. hardcoded for now is fine.
+const url = 'https://www.youtube.com/watch?v=WZsY5yYGbt8'; //put a url in here.. hardcoded for now is fine.
 
-let info = await ytdl.getInfo('https://www.youtube.com/watch?v=WZsY5yYGbt8');
+const outStream = fs.createWriteStream('sunday.wav');
+const inStream = await ytdl(url, {filter: 'audioonly'});
 
+inStream.pipe(outStream);
 
-console.info('ytdl info', info);
+outStream.on('close', () => {
+    console.info('Done');
+});
+
+//const info = await ytdl.getInfo(url);
+//console.info('ytdl info', info);
