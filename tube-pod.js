34acda739f3ed39cf1085ced9790f015ff860282
Elle Rose <ellrose@pop-os.localdomain>
2020-10-06 09:42:56 -0400

    downloads an mp3 file, audio only

--- tube-pod.js
+++ tube-pod.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import ytdl from 'ytdl-core';
-import fs from 'fs';
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 
 const url = 'https://www.youtube.com/watch?v=WZsY5yYGbt8'; //put a url in here.. hardcoded for now is fine.
 
-const outStream = fs.createWriteStream('sunday.wav');
-const inStream = await ytdl(url, {filter: 'audioonly'});
+const outStream = fs.createWriteStream('sunday.mp3');
+const inStream = ytdl(url, { quality: 'highest', filter: 'audioonly'});
 
 inStream.pipe(outStream);
 
